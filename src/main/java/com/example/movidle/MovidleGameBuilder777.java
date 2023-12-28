106883c001612f4a783a5d48469bcabdbd234439
Furky <77547158+coderfeye13@users.noreply.github.com>
2023-12-28 16:31:03 +0300

    Update MovidleGameBuilder777.java

--- src/main/java/com/example/movidle/MovidleGameBuilder777.java
+++ src/main/java/com/example/movidle/MovidleGameBuilder777.java
@@ -2,9 +2,5 @@ package com.example.movidle;
 
 public class MovidleGameBuilder777 {
     /*----------ALGORİTMA KODLARI BURAYAAA-----
-    bu classta sanırım oyunun ana hatlarını içeren kodlar olmalı.
-    Ahmet 312 satır kod yazmış buraya amk yuh. Gerçi özgenc de burayı sanırım core altında
-    toplayıp zibilyon tane şey yapmışlar.
-
     */
 }
